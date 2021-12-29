831e3f9d24a42c312b918cea013291fcde4ee2f2
Byron 2016 <bgva2005@yahoo.com>
2021-12-28 23:01:56 -0500

    fix: add AddJWTTokenServices service to program.cs

--- JwtAutDemo/Program.cs
+++ JwtAutDemo/Program.cs
@@ -1,6 +1,9 @@
+using JwtAutDemo.Areas.jwt.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddJWTTokenServices(builder.Configuration);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
